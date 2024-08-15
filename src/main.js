d276d5373499b0d0bdc881918abea734ff458f53
deanhopes <deanhopes@gmail.com>
2024-08-15 07:23:39 +0100

    init

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@
 // ▄█ ░█░ █▀█ █▀▄ ░█░ ██▄ █▀▄   ░█░ ██▄ █░▀░█ █▀▀ █▄▄ █▀█ ░█░ ██▄
 
 import "./styles/style.css";
-// import Lenis from "@studio-freight/lenis"; // TODO Add lenis smooth scrolling 
-// import gsap from 'gsap'  // TODO [import GSAP have to import in the function's js file]
+import Lenis from "@studio-freight/lenis"; // TODO Add lenis smooth scrolling 
+import gsap from 'gsap'  // TODO [import GSAP have to import in the function's js file]
 
 import home from "./pages/home/home";
 import homeScroll from "./pages/home/home-scroll";
 
 // import ScrollTrigger from 'gsap/ScrollTrigger'  // TODO [import GSAP have to import in the function's js file]
-import home from './pages/home/home'  // [imprort js file to main.js]
+// import home from './pages/home/home'  // [imprort js file to main.js]
 
 // TODO don't forget to register plugins if you want to use GSAP
 //gsap.registerPlugin(ScrollTrigger, Flip);
@@ -20,19 +20,20 @@ import home from './pages/home/home'  // [imprort js file to main.js]
 // GLOBAL SCRIPTS
 
 // Lenis SmoothScroll
-// function scroll()
-// {
-//     const lenis = new Lenis()
+function scroll()
+{
+    const lenis = new Lenis()
 
-//         function raf(time) {
-//             lenis.raf(time)
-//             requestAnimationFrame(raf)
-//         }
+        function raf(time) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
 
-//     requestAnimationFrame(raf)
-// }
-// scroll();
+    requestAnimationFrame(raf)
+}
+scroll();
 
+console.log('working')
 
 // PAGE RELATED SCRIPTS
 const isHome = document.querySelector("body").classList.contains("body--home");
